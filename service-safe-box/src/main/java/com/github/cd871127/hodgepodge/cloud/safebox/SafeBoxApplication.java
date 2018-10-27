0078aa50edd9e9e71cf406f05a9aa71398ea4243
Anthony <cdistc@163.com>
2018-10-27 13:05:21 +0800

    pagehelper

---
+++ service-safe-box/src/main/java/com/github/cd871127/hodgepodge/cloud/safebox/SafeBoxApplication.java
@@ -0,0 +1,11 @@
+package com.github.cd871127.hodgepodge.cloud.safebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SafeBoxApplication.class, args);
+    }
+}
