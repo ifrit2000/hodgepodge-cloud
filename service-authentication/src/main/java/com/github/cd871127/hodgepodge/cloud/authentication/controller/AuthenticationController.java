6df030d48087af70f0b3e97fa39435936da9508f
Anthony <cdistc@163.com>
2018-10-29 16:24:53 +0800

    readme

--- service-authentication/src/main/java/com/github/cd871127/hodgepodge/cloud/authentication/controller/AuthenticationController.java
+++ service-authentication/src/main/java/com/github/cd871127/hodgepodge/cloud/authentication/controller/AuthenticationController.java
@@ -3,9 +3,7 @@ package com.github.cd871127.hodgepodge.cloud.authentication.controller;
 import com.github.cd871127.hodgepodge.cloud.authentication.service.AuthenticationService;
 import com.github.cd871127.hodgepodge.cloud.lib.user.UserInfo;
 import com.github.cd871127.hodgepodge.cloud.lib.web.AbstractController;
-
 import com.github.cd871127.hodgepodge.cloud.lib.web.server.response.CommonResponseInfo;
-import com.github.cd871127.hodgepodge.cloud.lib.web.server.response.ResponseInfo;
 import com.github.cd871127.hodgepodge.cloud.lib.web.server.response.ServerResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- service-authentication/src/main/java/com/github/cd871127/hodgepodge/cloud/authentication/mapper/AuthenticationMapper.java
+++ service-authentication/src/main/java/com/github/cd871127/hodgepodge/cloud/authentication/mapper/AuthenticationMapper.java
@@ -2,7 +2,6 @@ package com.github.cd871127.hodgepodge.cloud.authentication.mapper;
 
 import com.github.cd871127.hodgepodge.cloud.lib.user.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface AuthenticationMapper {
