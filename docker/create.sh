c6e633e903257e9900c32bbad99224ddf7abb3d5
Anthony <cdistc@163.com>
2018-12-14 16:26:34 +0800

    rsa

--- docker/create.sh
+++ docker/create.sh
@@ -6,10 +6,14 @@ docker pull mysql:8.0.13
 
 docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
+
 network_name=dev-network
+ipAddress=172.18.0.0
 
 #创建网络
-docker network create --subnet=172.18.0.0/16 ${network_name}
+docker network create --subnet=${ipAddress}/16 ${network_name}
+
+route -p add ${ipAddress} MASK 255.255.255.0 10.0.75.2
 
 #创建容器
 docker create --name dev-redis --network ${network_name} --ip 172.18.0.2 redis
