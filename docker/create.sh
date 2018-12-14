9ec3a7964cced6f44fe58cf7900a31cff3f96ba1
Anthony <cdistc@163.com>
2018-12-14 17:39:02 +0800

    rsa

--- docker/create.sh
+++ docker/create.sh
@@ -16,5 +16,5 @@ docker network create --subnet=${ipAddress}/16 ${network_name}
 route -p add ${ipAddress} MASK 255.255.255.0 10.0.75.2
 
 #创建容器
-docker create --name dev-redis --network ${network_name} --ip 172.18.0.2 redis
-docker create --name dev-consul --network ${network_name} --ip 172.18.0.3 consul
+docker create --name dev-redis --network ${network_name} --ip 172.18.0.2 redis:5.0.2
+docker create --name dev-consul --network ${network_name} --ip 172.18.0.3 consul:1.4.0
