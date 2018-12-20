4e0e428973e50b5be04f17c773eda1b064faa639
Anthony <cdistc@163.com>
2018-12-20 22:39:39 +0800

    docker

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+sudo groupadd docker     #添加docker用户组
+sudo gpasswd -a $USER docker     #将登陆用户加入到docker用户组中
+newgrp docker     #更新用户组
+docker ps    #测试docker命令是否可以使用sudo正常使用
+
 ./0-clean.sh
 ./1-image.sh
 ./2-network.sh
