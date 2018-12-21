4ccbe45e053fd80cdb85a61b6d2a0a9a62982ddb
Anthony <cdistc@163.com>
2018-12-21 22:33:25 +0800

    dockerfile

--- hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/charset/CharsetAutoConfiguration.java
+++ hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/charset/CharsetAutoConfiguration.java
@@ -2,7 +2,6 @@ package io.github.cd871127.hodgepodge.charset;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sun.nio.cs.UTF_8;
 
 import java.nio.charset.Charset;
 
@@ -10,6 +9,6 @@ import java.nio.charset.Charset;
 public class CharsetAutoConfiguration {
     @Bean
     public Charset charset() {
-        return UTF_8.INSTANCE;
+        return Charset.forName("UTF-8");
     }
 }
