2f74beb5c4aadb491836d0b2a7d2ea2284a95fe7
Anthony <cdistc@163.com>
2018-12-21 14:25:02 +0800

    format

--- service-cipher/src/main/java/com/github/cd871127/hodgepodge/cloud/cipher/CipherApplication.java
+++ service-cipher/src/main/java/com/github/cd871127/hodgepodge/cloud/cipher/CipherApplication.java
@@ -2,7 +2,6 @@ package com.github.cd871127.hodgepodge.cloud.cipher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
@@ -11,5 +10,4 @@ public class CipherApplication {
     public static void main(String[] args) {
         SpringApplication.run(CipherApplication.class, args);
     }
-
 }
