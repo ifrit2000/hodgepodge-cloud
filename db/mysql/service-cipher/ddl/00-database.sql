a46a2d72b8672e10cce4a4e01b44773481b498a9
anthony <cdistc@163.com>
2018-12-24 00:34:01 +0800

    db

--- db/mysql/service-cipher/ddl/00-database.sql
+++ db/mysql/service-cipher/ddl/00-database.sql
@@ -1,9 +1,4 @@
-CREATE USER 'appdata'@'%' IDENTIFIED BY '123456';
+CREATE DATABASE IF NOT EXISTS service_cipher DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 grant all privileges on service_cipher.* to 'appdata'@'%';
-
-CREATE USER 'appopr'@'%' IDENTIFIED BY '123456';
 grant select,delete,update on service_cipher.* to 'appopr'@'%';
-
-
-
 flush privileges;
--- mysql/create.sql
+++
@@ -1,5 +0,0 @@
-use safe_box;
-create table TEST(
-Id int,
-Name varchar(18)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
