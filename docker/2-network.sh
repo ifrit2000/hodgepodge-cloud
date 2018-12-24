3f62479c059a4fadac1084c23f5fb20ed73e0ffe
Anthony <cdistc@163.com>
2018-12-24 09:45:49 +0800

    net

--- docker/2-network.sh
+++ docker/2-network.sh
@@ -8,7 +8,7 @@ docker network create --subnet=${ipAddress}/16 ${network_name}
 #添加路由
 route -p add ${ipAddress} MASK 255.255.255.0 10.0.75.2
 
-route add -net 11.0.0.0/8 gw 10.31.167.247 eth0
+
 
 sudo route add -net 172.18.0.0 netmask 255.255.255.0 gw 172.18.0.1
 
