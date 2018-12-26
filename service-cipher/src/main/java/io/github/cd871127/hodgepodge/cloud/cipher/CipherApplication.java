784ab28757e3b141a52ef9a93e97f81eccf877ff
Anthony <cdistc@163.com>
2018-12-26 17:36:11 +0800

    config

--- service-cipher/src/main/java/io/github/cd871127/hodgepodge/cloud/cipher/CipherApplication.java
+++ service-cipher/src/main/java/io/github/cd871127/hodgepodge/cloud/cipher/CipherApplication.java
@@ -1,15 +1,11 @@
 package io.github.cd871127.hodgepodge.cloud.cipher;
 
-import io.github.cd871127.hodgepodge.cloud.cipher.properties.Cipher2Properties;
-import io.github.cd871127.hodgepodge.cloud.cipher.properties.CipherProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableConfigurationProperties({CipherProperties.class, Cipher2Properties.class})
 //http://springcloud.cn/view/415
 public class CipherApplication {
     public static void main(String[] args) {
