bbaf17c7e04819abe149cdc2f6f8cbb8c7ed9e90
Anthony <cdistc@163.com>
2018-12-26 16:27:13 +0800

    git2consul

--- service-gateway/src/main/java/io/github/cd871127/hodgepodge/cloud/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/io/github/cd871127/hodgepodge/cloud/gateway/GatewayApplication.java
@@ -1,15 +1,31 @@
 package io.github.cd871127.hodgepodge.cloud.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * @author anthony
  */
 @SpringBootApplication
+@RestController
+
 public class GatewayApplication {
+//    @Value("${spring.cloud.consul.config.prefix}")
+    @Value("${test.aaaa}")
+    private String bbb;
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @RequestMapping("test")
+    public String test() {
+        return bbb;
     }
 }
