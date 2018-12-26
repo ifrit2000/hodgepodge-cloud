46e23a129d4af713eaf528ea55c7422b0b444e86
Anthony <cdistc@163.com>
2018-12-26 12:46:51 +0800

    gateway

--- service-gateway/src/main/java/io/github/cd871127/hodgepodge/cloud/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/io/github/cd871127/hodgepodge/cloud/gateway/GatewayApplication.java
@@ -1,13 +1,13 @@
 package io.github.cd871127.hodgepodge.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 /**
  * @author anthony
  */
-@SpringCloudApplication
+@SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
