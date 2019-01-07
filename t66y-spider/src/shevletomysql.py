fe58b30aa534445215a4f9998a27a8f91eac0064
Anthony <cdistc@163.com>
2019-01-07 16:17:33 +0800

    t66y

--- t66y-spider/src/shevletomysql.py
+++ t66y-spider/src/shevletomysql.py
@@ -1,45 +1,81 @@
+import os
+import shelve
+import traceback
+
 import pymysql
 
 
-def insert_topic(connection, topics=None):
-    if topics is None:
+def insert_topic(connection, topic_list=None):
+    if topic_list is None:
         return None
+    topic_sql_template = """insert into t66y.TOPIC_INFO(TOPIC_URL, TOPIC_AREA, TOPIC_TITLE, TOPIC_STATUS) 
+            VALUES('%s','%s','%s','%s')"""
+    images_sql_template = """insert into t66y.IMAGE_INFO(TOPIC_URL, IMAGE_URL) 
+            VALUE %s"""
+    torrent_sql_template = """insert into t66y.TORRENT_INFO(TOPIC_URL, TORRENT_URL, TORRENT_HASH) 
+            VALUE %s"""
+    html_sql_template = """insert into t66y.HTML_INFO(TOPIC_URL, HTML) 
+            VALUES('%s','%s')"""
     with connection.cursor() as cursor:
-        topic_sql_template = """insert into t66y.TOPIC_INFO(TOPIC_URL, TOPIC_AREA, TOPIC_TITLE) 
-            VALUES('%s','%s','%s')"""
-        images_sql_template = """insert into t66y.IMAGE_INFO(TOPIC_URL, IMAGE_URL) 
-            VALUE %s"""
-        torrent_sql_template = """insert into t66y.TORRENT_INFO(TOPIC_URL, TORRENT_URL, TORRENT_HASH) 
-            VALUE %s"""
-
-        for topic in topics:
-            cursor.execute(topic_sql_template % (topic.get("url"), topic.area, topic.title))
+        for topic in topic_list:
+            # print(topic_sql_template % (topic.get("url"), topic.get("area"), topic.get("title")))
+            cursor.execute(
+                topic_sql_template % (topic.get("url"), topic.get("area"), topic.get("title"), topic.get("status")))
             image_value = ""
-            for image in topic.images:
-                image_value = image_value + "('%s','%s')," % (topic.url, image)
+            for image in topic.get("images", []):
+                image_value = image_value + "('%s','%s')," % (topic.get("url"), image)
             if image_value != "":
                 sql = (images_sql_template % image_value).rstrip(",")
                 cursor.execute(sql)
 
             torrent_value = ""
-            for torrent_link in topic.torrent_links:
+            for torrent_link in topic.get("torrent_links", []):
                 torrent_hash = torrent_link[torrent_link.find("hash=") + len("hash="):]
-                torrent_value = torrent_value + "('%s','%s','%s')," % (topic.url, torrent_link, torrent_hash)
+                torrent_value = torrent_value + "('%s','%s','%s')," % (topic.get("url"), torrent_link, torrent_hash)
             if torrent_value != "":
                 sql = (torrent_sql_template % torrent_value).rstrip(",")
                 cursor.execute(sql)
-            cursor.commit()
+            if topic["status"] != "0" and topic["html"] is not None:
+                sql = html_sql_template % (topic.get("url"), pymysql.escape_string(topic.get("html")))
+                cursor.execute(sql)
 
 
 if __name__ == '__main__':
-    connection = pymysql.connect(host="172.28.0.4",
-                                 user='appdata',
-                                 password='123456',
-                                 db='t66y',
-                                 charset='utf8',
-                                 cursorclass=pymysql.cursors.DictCursor,
-                                 use_unicode=True)
-    connection.close()
-    a = dict()
-    a["url"] = 12
-    print(a.get("url", "1"))
+    con = pymysql.connect(host="172.28.0.4",
+                          port=3306,
+                          user='appdata',
+                          password='123456',
+                          db='t66y',
+                          charset='utf8',
+                          cursorclass=pymysql.cursors.DictCursor,
+                          use_unicode=True)
+    topic_dict = {}
+    try:
+        for fid in ["2", "4", "5", "15", "25", "26", "27"]:
+            with shelve.open(os.path.join("/home/anthony/Desktop/data", fid)) as db:
+                for page_num in range(1, 101):
+                    topic_list = db.get(str(page_num), [])
+                    for topic in topic_list:
+                        topic["area"] = fid
+                        if "'" in topic["title"]:
+                            topic["title"] = pymysql.escape_string(topic["title"])
+                        topic_dict[topic["url"]] = topic
+                        if topic.get("status", "0") == "0":
+                            topic["status"] = "0"
+                            topic["html"] = None
+                        else:
+                            topic["status"] = "1"
+            print("fid %s load completed" % fid)
+        # with shelve.open(os.path.join("/home/anthony/Desktop/data", "all")) as db2:
+        #     db2["data"] = topic_dict
+        print("write db")
+        insert_topic(con, list(topic_dict.values()))
+        print("finish write")
+        con.commit()
+        print("committed")
+    except Exception as e:
+        con.rollback()
+        print(traceback.format_exc())
+        pass
+    finally:
+        con.close()
