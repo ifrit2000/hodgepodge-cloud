4af387e61508aa176fd9f912ff91059753277679
Anthony <cdistc@163.com>
2019-01-10 16:02:21 +0800

    mybatis interceptor

--- hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/mybatis/autoconfigure/MybatisInterceptorAutoConfiguration.java
+++ hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/mybatis/autoconfigure/MybatisInterceptorAutoConfiguration.java
@@ -1,11 +1,16 @@
 package io.github.cd871127.hodgepodge.mybatis.autoconfigure;
 
 import io.github.cd871127.hodgepodge.mybatis.SqlLogInterceptor;
+import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass({MappedStatement.class, MapperMethod.class, Interceptor.class})
 public class MybatisInterceptorAutoConfiguration {
 
     @Bean
