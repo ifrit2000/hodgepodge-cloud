855625051aabe8c68877e0aa5506cdaaced0889c
anthony <cdistc@163.com>
2019-07-01 01:21:55 +0800

    docker compose

--- hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/mybatis/autoconfigure/MybatisInterceptorAutoConfiguration.java
+++ hodgepodge-spring-boot-starter/src/main/java/io/github/cd871127/hodgepodge/mybatis/autoconfigure/MybatisInterceptorAutoConfiguration.java
@@ -1,9 +1,12 @@
 package io.github.cd871127.hodgepodge.mybatis.autoconfigure;
 
 import io.github.cd871127.hodgepodge.mybatis.SqlLogInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -18,8 +21,10 @@ public class MybatisInterceptorAutoConfiguration {
     @ConditionalOnMissingBean
     @ConditionalOnProperty(prefix = "mybatis", value = "sql-log", havingValue = "true", matchIfMissing = true)
     SqlLogInterceptor sqlLogInterceptor() {
+
         return new SqlLogInterceptor();
     }
 
 
 }
+
