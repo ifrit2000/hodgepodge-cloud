4bb23618bfc69953648e54e2ef5210efe5b17e09
Anthony <cdistc@163.com>
2019-07-05 15:08:43 +0800

    account

---
+++ service-account/db/02-database-read.sql
@@ -0,0 +1,1 @@
+CREATE USER 'accountread'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
---
+++ service-account/src/main/java/io/github/cd871127/hodgepodge/cloud/account/AccountApplication.java
@@ -0,0 +1,11 @@
+package io.github.cd871127.hodgepodge.cloud.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class);
+    }
+}
