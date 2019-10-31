29f144fda214b291d194b60f7565ee7b54902cb5
anthonychen <anthonychen@lexin.com>
2019-10-31 18:52:00 +0800

    1

--- service-shiro/src/main/java/io/github/cd871127/hodgepodge/cloud/shiro/ShiroController.java
+++ service-shiro/src/main/java/io/github/cd871127/hodgepodge/cloud/shiro/ShiroController.java
@@ -1,10 +1,23 @@
 package io.github.cd871127.hodgepodge.cloud.shiro;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Anthony Chen
  * @date 2019/10/29
  **/
-
+@RestController
+@RequestMapping("/test")
 public class ShiroController {
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
 
+    @GetMapping("test2")
+    public String test2() {
+        return "test2";
+    }
 }
